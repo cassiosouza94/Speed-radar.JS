3d3419b042c51c0ce05989a3910ea3256396a1c7
Cássio Almeida Souza <cassioasouzaa@gmail.com>
2022-04-13 19:25:18 -0300

    My code in JavaScript

---
+++ Speed_control_systen.js
@@ -0,0 +1,6 @@
+var velocidade = 120
+console.log("A velocidade do seu carr foi de", + velocidade,"Km/h")
+if(velocidade > 100){
+console.log("🚨Você ultrapassou o limite de velocidade permitida na Rodovia Anhanguera SP-330. Multado!🚨") 
+}
+console.log("Dirija sempre com o cinto de segurança! 👮‍♂️")
